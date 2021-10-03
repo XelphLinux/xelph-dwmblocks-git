59b3b81e88320317ebe1a05951f2aca9e9e0cf03
AakashSharma7269 <aakashsensharma@gmail.com>
2021-10-03 11:11:12 +0530

    initial upload

---
+++ opt/xelph-dwmblocks-git/blocks.def.h
@@ -0,0 +1,12 @@
+static const Block blocks[] = {
+	// {"Icon", "Command", "update interval", "update signal"}
+	{"", "/opt/xelph-dwmblocks-git/scripts/packages",120,0},
+	{"", "/opt/xelph-dwmblocks-git/scripts/battery",1,0},
+	{"", "/opt/xelph-dwmblocks-git/scripts/volume",1,0},
+	{"", "/opt/xelph-dwmblocks-git/scripts/brightness",1,0},
+	{"", "/opt/xelph-dwmblocks-git/scripts/cpu",1,0},
+	{"", "/opt/xelph-dwmblocks-git/scripts/memory",1,0},
+	{"", "/opt/xelph-dwmblocks-git/scripts/clock",1,0},
+
+};
+static char delim = ' ';
