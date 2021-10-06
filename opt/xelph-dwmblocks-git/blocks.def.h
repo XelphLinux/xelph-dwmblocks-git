bbe11c3ee764961d939d5407c15dcd89dbc3014d
AakashSharma7269 <aakashsensharma@gmail.com>
2021-10-06 10:47:03 +0530

    removed volume

--- opt/xelph-dwmblocks-git/blocks.def.h
+++ opt/xelph-dwmblocks-git/blocks.def.h
@@ -2,7 +2,6 @@ static const Block blocks[] = {
 	// {"Icon", "Command", "update interval", "update signal"}
 	{"", "/opt/xelph-dwmblocks-git/scripts/packages",120,0},
 	{"", "/opt/xelph-dwmblocks-git/scripts/battery",1,0},
-	{"", "/opt/xelph-dwmblocks-git/scripts/volume",1,0},
 	{"", "/opt/xelph-dwmblocks-git/scripts/brightness",1,0},
 	{"", "/opt/xelph-dwmblocks-git/scripts/cpu",1,0},
 	{"", "/opt/xelph-dwmblocks-git/scripts/memory",1,0},
